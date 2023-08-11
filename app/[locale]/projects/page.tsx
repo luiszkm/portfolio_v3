1bbc26934e10a2e63c1130ae77cb973355a45517
luiszkm <luiszkm@gmail.com>
2023-08-10 22:22:56 -0300

    feat:add cardProject

--- app/[locale]/projects/page.tsx
+++ app/[locale]/projects/page.tsx
@@ -1,20 +1,12 @@
-'use client'
-import { useParams, usePathname } from "next/navigation"
-
-interface IProjects {
-  params: {
-    slug: string
-  }
-}
-
-
 export default function Projects () {
-  const p =usePathname()
-  console.log(p);
+  
   
   return (
-    <div className="flex items-center gap-4 p-3 w-full">
-      aaaa
+    <div className="min-h-full">
+      <div>
+        <h1 className="text-4xl font-bold text-center">Project Name</h1>
+        
+      </div>
     </div>
   )
 }
