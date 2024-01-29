1ffb433ff85f15486c55d91e40d6f911ebb1f4d0
luiszkm <luiszkm@gmail.com>
2024-01-29 14:01:56 -0300

    fix: fix button and footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,10 +5,12 @@ export function Footer() {
   const t = useTranslations('Index')
 
   return (
-      <footer className="w-full flex flex-col h-10 items-center justify-center
+      <footer className="w-full flex flex-col h-12 items-center justify-center
       border-t border-gray-400
-      bg-gradient-to-t from-gray-300 via-gray-200 to-gray-100
-      dark:bg-gradient-to-b dark:from-gray-900 dark:to-gray-400 dark:via-gray-700 ">
+      bg-gradient-to-t from-gray-300
+       via-gray-200 to-gray-100
+      dark:bg-gradient-to-t dark:from-gray-900 
+      dark:border-none">
         <small>DevSoares Belo Horizonte, MG , Brasil</small>
         
       </footer>
